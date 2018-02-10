f41fbae247990ba2a7c1ce7663eca1f6a59360df
Brian Coventry <bcov@uw.edu>
2018-02-10 10:07:50 -0800

    Initial commit

---
+++ pyrosetta_ghost/__init__.py
@@ -0,0 +1,1 @@
+import pyrosetta_ghost
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__version__ = "1.0"
+
+setup(
+   name='pyrosetta_ghost',
+   version='1.0',
+   description='never let it die',
+   author='bcov',
+   author_email='bcov@uw.edu',
+   packages=['pyrosetta_ghost'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
