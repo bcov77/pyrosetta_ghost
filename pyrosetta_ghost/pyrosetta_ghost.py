22d039830119bd8add6716660e6ac055ac1254d6
Brian Coventry <bcov@digs.baker>
2018-02-10 10:36:19 -0800

    Added correct namespaces for things not in minimization_packing

--- pyrosetta_ghost/pyrosetta_ghost.py
+++ pyrosetta_ghost/pyrosetta_ghost.py
@@ -1,8 +1,6 @@
 
 
 import pyrosetta
-from pyrosetta import *
-from pyrosetta.rosetta import *
 
 try:
     m = pyrosetta.rosetta.protocols.simple_moves.MinMover
@@ -10,77 +8,28 @@ except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.MinMover = pyrosetta.rosetta.protocols.minimization_packing.MinMover
 
 try:
-    m = rosetta.protocols.simple_moves.MinMover
-except AttributeError:
-    rosetta.protocols.simple_moves.MinMover = rosetta.protocols.minimization_packing.MinMover
-
-try:
-    m = protocols.simple_moves.MinMover
-except AttributeError:
-    protocols.simple_moves.MinMover = protocols.minimization_packing.MinMover
-
-try:
     m = pyrosetta.rosetta.protocols.simple_moves.BoltzmannRotamerMover
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.BoltzmannRotamerMover = pyrosetta.rosetta.protocols.minimization_packing.BoltzmannRotamerMover
-
-try:
-    m = rosetta.protocols.simple_moves.BoltzmannRotamerMover
-except AttributeError:
-    rosetta.protocols.simple_moves.BoltzmannRotamerMover = rosetta.protocols.minimization_packing.BoltzmannRotamerMover
-
-try:
-    m = protocols.simple_moves.BoltzmannRotamerMover
-except AttributeError:
-    protocols.simple_moves.BoltzmannRotamerMover = protocols.minimization_packing.BoltzmannRotamerMover
 
 
 try:
     m = pyrosetta.rosetta.protocols.simple_moves.EvolutionaryDynamicsMover
 except AttributeError:
-    pyrosetta.rosetta.protocols.simple_moves.EvolutionaryDynamicsMover = pyrosetta.rosetta.protocols.minimization_packing.EvolutionaryDynamicsMover
+    pyrosetta.rosetta.protocols.simple_moves.EvolutionaryDynamicsMover = pyrosetta.rosetta.protocols.evolution.EvolutionaryDynamicsMover
 
-try:
-    m = rosetta.protocols.simple_moves.EvolutionaryDynamicsMover
-except AttributeError:
-    rosetta.protocols.simple_moves.EvolutionaryDynamicsMover = rosetta.protocols.minimization_packing.EvolutionaryDynamicsMover
-
-try:
-    m = protocols.simple_moves.EvolutionaryDynamicsMover
-except AttributeError:
-    protocols.simple_moves.EvolutionaryDynamicsMover = protocols.minimization_packing.EvolutionaryDynamicsMover
 
 
 try:
     m = pyrosetta.rosetta.protocols.simple_moves.ExtendedPoseMover
 except AttributeError:
-    pyrosetta.rosetta.protocols.simple_moves.ExtendedPoseMover = pyrosetta.rosetta.protocols.minimization_packing.ExtendedPoseMover
-
-try:
-    m = rosetta.protocols.simple_moves.ExtendedPoseMover
-except AttributeError:
-    rosetta.protocols.simple_moves.ExtendedPoseMover = rosetta.protocols.minimization_packing.ExtendedPoseMover
-
-try:
-    m = protocols.simple_moves.ExtendedPoseMover
-except AttributeError:
-    protocols.simple_moves.ExtendedPoseMover = protocols.minimization_packing.ExtendedPoseMover
+    pyrosetta.rosetta.protocols.simple_moves.ExtendedPoseMover = pyrosetta.rosetta.protocols.pose_creation.ExtendedPoseMover
 
 
 try:
     m = pyrosetta.rosetta.protocols.simple_moves.ExtendedPoseMoverCreator
 except AttributeError:
-    pyrosetta.rosetta.protocols.simple_moves.ExtendedPoseMoverCreator = pyrosetta.rosetta.protocols.minimization_packing.ExtendedPoseMoverCreator
-
-try:
-    m = rosetta.protocols.simple_moves.ExtendedPoseMoverCreator
-except AttributeError:
-    rosetta.protocols.simple_moves.ExtendedPoseMoverCreator = rosetta.protocols.minimization_packing.ExtendedPoseMoverCreator
-
-try:
-    m = protocols.simple_moves.ExtendedPoseMoverCreator
-except AttributeError:
-    protocols.simple_moves.ExtendedPoseMoverCreator = protocols.minimization_packing.ExtendedPoseMoverCreator
+    pyrosetta.rosetta.protocols.simple_moves.ExtendedPoseMoverCreator = pyrosetta.rosetta.protocols.pose_creation.ExtendedPoseMoverCreator
 
 
 try:
@@ -88,31 +37,13 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.GreenPacker = pyrosetta.rosetta.protocols.minimization_packing.GreenPacker
 
-try:
-    m = rosetta.protocols.simple_moves.GreenPacker
-except AttributeError:
-    rosetta.protocols.simple_moves.GreenPacker = rosetta.protocols.minimization_packing.GreenPacker
-
-try:
-    m = protocols.simple_moves.GreenPacker
-except AttributeError:
-    protocols.simple_moves.GreenPacker = protocols.minimization_packing.GreenPacker
 
 
 try:
     m = pyrosetta.rosetta.protocols.simple_moves.MakePolyXMover
 except AttributeError:
-    pyrosetta.rosetta.protocols.simple_moves.MakePolyXMover = pyrosetta.rosetta.protocols.minimization_packing.MakePolyXMover
+    pyrosetta.rosetta.protocols.simple_moves.MakePolyXMover = pyrosetta.rosetta.protocols.pose_creation.MakePolyXMover
 
-try:
-    m = rosetta.protocols.simple_moves.MakePolyXMover
-except AttributeError:
-    rosetta.protocols.simple_moves.MakePolyXMover = rosetta.protocols.minimization_packing.MakePolyXMover
-
-try:
-    m = protocols.simple_moves.MakePolyXMover
-except AttributeError:
-    protocols.simple_moves.MakePolyXMover = protocols.minimization_packing.MakePolyXMover
 
 
 try:
@@ -120,15 +51,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.MinMover = pyrosetta.rosetta.protocols.minimization_packing.MinMover
 
-try:
-    m = rosetta.protocols.simple_moves.MinMover
-except AttributeError:
-    rosetta.protocols.simple_moves.MinMover = rosetta.protocols.minimization_packing.MinMover
-
-try:
-    m = protocols.simple_moves.MinMover
-except AttributeError:
-    protocols.simple_moves.MinMover = protocols.minimization_packing.MinMover
 
 
 try:
@@ -136,15 +58,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.MinPackMover = pyrosetta.rosetta.protocols.minimization_packing.MinPackMover
 
-try:
-    m = rosetta.protocols.simple_moves.MinPackMover
-except AttributeError:
-    rosetta.protocols.simple_moves.MinPackMover = rosetta.protocols.minimization_packing.MinPackMover
-
-try:
-    m = protocols.simple_moves.MinPackMover
-except AttributeError:
-    protocols.simple_moves.MinPackMover = protocols.minimization_packing.MinPackMover
 
 
 try:
@@ -152,15 +65,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.PackRotamersMoverLazy = pyrosetta.rosetta.protocols.minimization_packing.PackRotamersMoverLazy
 
-try:
-    m = rosetta.protocols.simple_moves.PackRotamersMoverLazy
-except AttributeError:
-    rosetta.protocols.simple_moves.PackRotamersMoverLazy = rosetta.protocols.minimization_packing.PackRotamersMoverLazy
-
-try:
-    m = protocols.simple_moves.PackRotamersMoverLazy
-except AttributeError:
-    protocols.simple_moves.PackRotamersMoverLazy = protocols.minimization_packing.PackRotamersMoverLazy
 
 
 try:
@@ -168,15 +72,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.PeptideStapleMover = pyrosetta.rosetta.protocols.minimization_packing.PeptideStapleMover
 
-try:
-    m = rosetta.protocols.simple_moves.PeptideStapleMover
-except AttributeError:
-    rosetta.protocols.simple_moves.PeptideStapleMover = rosetta.protocols.minimization_packing.PeptideStapleMover
-
-try:
-    m = protocols.simple_moves.PeptideStapleMover
-except AttributeError:
-    protocols.simple_moves.PeptideStapleMover = protocols.minimization_packing.PeptideStapleMover
 
 
 try:
@@ -184,15 +79,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.PertMinMover = pyrosetta.rosetta.protocols.minimization_packing.PertMinMover
 
-try:
-    m = rosetta.protocols.simple_moves.PertMinMover
-except AttributeError:
-    rosetta.protocols.simple_moves.PertMinMover = rosetta.protocols.minimization_packing.PertMinMover
-
-try:
-    m = protocols.simple_moves.PertMinMover
-except AttributeError:
-    protocols.simple_moves.PertMinMover = protocols.minimization_packing.PertMinMover
 
 
 try:
@@ -200,15 +86,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.RepackSidechainsMover = pyrosetta.rosetta.protocols.minimization_packing.RepackSidechainsMover
 
-try:
-    m = rosetta.protocols.simple_moves.RepackSidechainsMover
-except AttributeError:
-    rosetta.protocols.simple_moves.RepackSidechainsMover = rosetta.protocols.minimization_packing.RepackSidechainsMover
-
-try:
-    m = protocols.simple_moves.RepackSidechainsMover
-except AttributeError:
-    protocols.simple_moves.RepackSidechainsMover = protocols.minimization_packing.RepackSidechainsMover
 
 
 try:
@@ -216,15 +93,6 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.RotamerTrialsMinMover = pyrosetta.rosetta.protocols.minimization_packing.RotamerTrialsMinMover
 
-try:
-    m = rosetta.protocols.simple_moves.RotamerTrialsMinMover
-except AttributeError:
-    rosetta.protocols.simple_moves.RotamerTrialsMinMover = rosetta.protocols.minimization_packing.RotamerTrialsMinMover
-
-try:
-    m = protocols.simple_moves.RotamerTrialsMinMover
-except AttributeError:
-    protocols.simple_moves.RotamerTrialsMinMover = protocols.minimization_packing.RotamerTrialsMinMover
 
 
 try:
@@ -232,31 +100,11 @@ try:
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.RotamerTrialsMover = pyrosetta.rosetta.protocols.minimization_packing.RotamerTrialsMover
 
-try:
-    m = rosetta.protocols.simple_moves.RotamerTrialsMover
-except AttributeError:
-    rosetta.protocols.simple_moves.RotamerTrialsMover = rosetta.protocols.minimization_packing.RotamerTrialsMover
-
-try:
-    m = protocols.simple_moves.RotamerTrialsMover
-except AttributeError:
-    protocols.simple_moves.RotamerTrialsMover = protocols.minimization_packing.RotamerTrialsMover
-
 
 try:
     m = pyrosetta.rosetta.protocols.simple_moves.RotamerizeMover
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.RotamerizeMover = pyrosetta.rosetta.protocols.minimization_packing.RotamerizeMover
-
-try:
-    m = rosetta.protocols.simple_moves.RotamerizeMover
-except AttributeError:
-    rosetta.protocols.simple_moves.RotamerizeMover = rosetta.protocols.minimization_packing.RotamerizeMover
-
-try:
-    m = protocols.simple_moves.RotamerizeMover
-except AttributeError:
-    protocols.simple_moves.RotamerizeMover = protocols.minimization_packing.RotamerizeMover
 
 
 try:
@@ -265,30 +113,10 @@ except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.SaneMinMover = pyrosetta.rosetta.protocols.minimization_packing.SaneMinMover
 
 try:
-    m = rosetta.protocols.simple_moves.SaneMinMover
-except AttributeError:
-    rosetta.protocols.simple_moves.SaneMinMover = rosetta.protocols.minimization_packing.SaneMinMover
-
-try:
-    m = protocols.simple_moves.SaneMinMover
-except AttributeError:
-    protocols.simple_moves.SaneMinMover = protocols.minimization_packing.SaneMinMover
-
-
-try:
     m = pyrosetta.rosetta.protocols.simple_moves.TaskAwareMinMover
 except AttributeError:
     pyrosetta.rosetta.protocols.simple_moves.TaskAwareMinMover = pyrosetta.rosetta.protocols.minimization_packing.TaskAwareMinMover
 
-try:
-    m = rosetta.protocols.simple_moves.TaskAwareMinMover
-except AttributeError:
-    rosetta.protocols.simple_moves.TaskAwareMinMover = rosetta.protocols.minimization_packing.TaskAwareMinMover
-
-try:
-    m = protocols.simple_moves.TaskAwareMinMover
-except AttributeError:
-    protocols.simple_moves.TaskAwareMinMover = protocols.minimization_packing.TaskAwareMinMover
 
 
 
